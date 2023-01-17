f7cb97994cde9d5dd815b38cf3376d7934c4d92a
mattygersh21 <dakotaseia@hotmail.com>
2023-01-16 23:14:41 -0600

    * Added clap dependency. * Added support for argument and flag when running program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{App,Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Matthew Gerszewski <dakotaseia@hotmail.com>")
+        .about("Clone of echo command-line application written in Rust.")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+    
+        print!("{:#?}", matches);
 }
